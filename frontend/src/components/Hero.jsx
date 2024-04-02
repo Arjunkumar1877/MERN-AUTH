ea93f6a36a01bdb800b4795641d64ca8b391baaa
Arjun kumar vs <arjun.tech177@gmail.com>
2024-04-03 05:05:48 +0530

    Routers  and login screen created

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import { Container, Card, Button } from "react-bootstrap";
+
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Hero = () => {
+  return (
+    <div className=" py-5">
+      <Container className="d-flex justify-content-center">
+        <Card className="p-5 d-flex flex-column align-items-center  hero-card bg-light w-75">
+          <h1 className="text-center mb-4">MERN AUTHENTICATION</h1>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consectetur, recusandae ex eaque dolore natus sapiente.
+          </p>
+          <div className="d-flex">
+            <LinkContainer  to={"/login"}>
+            <Button variant="primary" className="me-3">
+              Sign In
+            </Button>
+            
+            </LinkContainer>
+           <LinkContainer to={"/register"}>
+           <Button variant="secondary">
+             Sign Up
+            </Button>
+           </LinkContainer>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
